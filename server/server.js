2aadfb2d290ba09173afc8046e48ae9da5f3ca07
toyah1427 <toyahdegroat1427@gmail.com>
2024-04-12 16:41:09 +0100

    attempt to make comments and database info show

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 import express from "express";
 import cors from "cors";
+import Database from "better-sqlite3";
+
+const db = new Database("database.db");
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
@@ -12,9 +16,41 @@ app.get("/", function (request, response) {
     response.json("Youre looking at my root");
 });
 
-app.post("/message", function (request, response) {
-    const newMessage = request.body;
-    console.log(newMessage);
-    response.json(newMessage);
+app.get("/bgclasses", function (request, response) {
+    console.log("Showing BG3 classes");
+    const bgClasses = db.prepare("SELECT * FROM classes").all();
+    response.json(bgClasses);
+});
+
+
+app.get("/comments", function (request, response) {
+    console.log(request.query);
+    console.log("Loading user comments...");
+    const comments = db
+    .prepare(`SELECT * FROM comments WHERE name = ${request.query.id}`)
+    .all();
+    console.log(comments);
+    response.json(comments);
+});
+
+
+app.post("/comments", function (request, response) {
+    console.log(request.body);
+    response.json(request.body);
+    
+    const addtNewComment = db.prepare(`
+    INSERT INTO comments (name, comment) VALUES (?, ?)
+    `);
+
+    addNewComment.run(
+        request.body.name,
+        request.body.comment
+    );
+    if (err) {
+        console.error(err.message);
+        response.status(500).send("Error with adding comment");
+    } else {
+        response.send("Yippee");
+    }
 });
 
