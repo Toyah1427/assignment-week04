2f9d8ab0784860dacf78f40c2b1f564c29de52b0
toyah1427 <toyahdegroat1427@gmail.com>
2024-04-12 18:00:29 +0100

    changes to js client

--- client/app.js
+++ client/app.js
@@ -1,4 +1,15 @@
 const form = document.getElementById("messageform");
+const bgclasses = document.getElementById("bgclasses");
+
+async function getBGClasses() {
+    bgclasses.innerHTML = "";
+
+    console.log("app.js BG3 Classes");
+    const classesresponse = await fetch(
+        "https://message-board-lp85.onrender.com/"
+    );
+
+}
 
 function handleSubmit(event) {
 event.preventDefault();
@@ -8,7 +19,7 @@ const message = event.target.message.value;
 console.log({ username: username, message: message });
 
 
-fetch("http://localhost:3000/message", {
+fetch("https://message-board-lp85.onrender.com/comments", {
     method: "POST",
     body: JSON.stringify({ username: username, message: message }),
     headers: {
